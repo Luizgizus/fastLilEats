1041d4d521f605c3e12f391a921aea9e60d6e2a7
Unknown <luizhenriquesilvajesus@gmail.com>
2018-06-10 16:59:30 -0300

    corrigindo modelagem do banco

--- DATA BASE/comidinhas_rapidas_final.sql
+++ DATA BASE/comidinhas_rapidas_final.sql
@@ -1,3 +1,6 @@
+-- -----------------------------------------------------
+-- Schema comidinhas_rapidas
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `comidinhas_rapidas` DEFAULT CHARACTER SET utf8 ;
 USE `comidinhas_rapidas` ;
 
@@ -17,6 +20,7 @@ INSERT INTO garcon (nome, gorjeta, status) VALUES ("Marcia Fagunes", 0, "ativo")
 INSERT INTO garcon (nome, gorjeta, status) VALUES ("Rafael Fernandes", 0, "ativo");
 INSERT INTO garcon (nome, gorjeta, status) VALUES ("Nícolas Moura", 0, "ativo");
 
+
 -- -----------------------------------------------------
 -- Table `comidinhas_rapidas`.`produto`
 -- -----------------------------------------------------
@@ -30,6 +34,7 @@ CREATE TABLE IF NOT EXISTS `comidinhas_rapidas`.`produto` (
   PRIMARY KEY (`id_produto`))
 ENGINE = InnoDB;
 
+
 INSERT INTO produto (valor, nome, descricao, tipo, tempo_estimado) VALUES 
 (20.00, "strogonoff", "strogonoff de frango", "alimento", 40)
 ,(30.00, "porção de batata frita", "batata frita com queijo", "alimento", 20)
@@ -41,7 +46,6 @@ INSERT INTO produto (valor, nome, descricao, tipo, tempo_estimado) VALUES
 ,(12.00, "cerveja", "79% de teor de alcool", "alimento", 10)
 ,(100.00, "whisky", "Fortona", "alimento", 10)
 ,(4.00, "agua", "agua natural", "alimento", 10);
-
 
 -- -----------------------------------------------------
 -- Table `comidinhas_rapidas`.`mesa`
@@ -76,33 +80,32 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `comidinhas_rapidas`.`pedido` (
   `id_pedido` INT NOT NULL AUTO_INCREMENT,
-  `garcon_id_garcon` INT NOT NULL,
   `status` VARCHAR(45) NOT NULL,
+  `nomeCliente` VARCHAR(45) NOT NULL,
   `tempoEsperaTotal` INT NOT NULL,
-  `garcon_id_garcon1` INT NOT NULL,
-  `pagamento_id_pagamento` INT NOT NULL,
+  `garcon_id_garcon` INT NOT NULL,
+  `pagamento_id_pagamento` INT,
   `mesa_id_mesa` INT NOT NULL,
   PRIMARY KEY (`id_pedido`),
-  INDEX `fk_pedido_garcon_idx` (`garcon_id_garcon1` ASC),
-  INDEX `fk_pedido_pagamento1_idx` (`pagamento_id_pagamento` ASC),
-  INDEX `fk_pedido_mesa1_idx` (`mesa_id_mesa` ASC),
+  INDEX `fk_pedido_garcon_idx` (`garcon_id_garcon` ASC),
+  INDEX `fk_pedido_pagamento_idx` (`pagamento_id_pagamento` ASC),
+  INDEX `fk_pedido_mesa_idx` (`mesa_id_mesa` ASC),
   CONSTRAINT `fk_pedido_garcon`
-    FOREIGN KEY (`garcon_id_garcon1`)
+    FOREIGN KEY (`garcon_id_garcon`)
     REFERENCES `comidinhas_rapidas`.`garcon` (`id_garcon`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_pedido_pagamento1`
+  CONSTRAINT `fk_pedido_pagamento`
     FOREIGN KEY (`pagamento_id_pagamento`)
     REFERENCES `comidinhas_rapidas`.`pagamento` (`id_pagamento`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_pedido_mesa1`
+  CONSTRAINT `fk_pedido_mesa`
     FOREIGN KEY (`mesa_id_mesa`)
     REFERENCES `comidinhas_rapidas`.`mesa` (`id_mesa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `comidinhas_rapidas`.`cliente`
@@ -119,18 +122,18 @@ ENGINE = InnoDB;
 -- Table `comidinhas_rapidas`.`pagamento_cliente`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `comidinhas_rapidas`.`pagamento_cliente` (
+  `id_pagamento_cliente` INT NOT NULL AUTO_INCREMENT,
   `pagamento_id_pagamento` INT NOT NULL,
   `cliente_id_cliente` INT NOT NULL,
-  `id_pagamento_cliente` INT NOT NULL AUTO_INCREMENT,
-  INDEX `fk_pagamento_has_cliente_cliente1_idx` (`cliente_id_cliente` ASC),
-  INDEX `fk_pagamento_has_cliente_pagamento1_idx` (`pagamento_id_pagamento` ASC),
+  INDEX `fk_pagamento_has_cliente_cliente_idx` (`cliente_id_cliente` ASC),
+  INDEX `fk_pagamento_has_cliente_pagamento_idx` (`pagamento_id_pagamento` ASC),
   PRIMARY KEY (`id_pagamento_cliente`),
-  CONSTRAINT `fk_pagamento_has_cliente_pagamento1`
+  CONSTRAINT `fk_pagamento_has_cliente_pagamento`
     FOREIGN KEY (`pagamento_id_pagamento`)
     REFERENCES `comidinhas_rapidas`.`pagamento` (`id_pagamento`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_pagamento_has_cliente_cliente1`
+  CONSTRAINT `fk_pagamento_has_cliente_cliente`
     FOREIGN KEY (`cliente_id_cliente`)
     REFERENCES `comidinhas_rapidas`.`cliente` (`id_cliente`)
     ON DELETE NO ACTION
@@ -142,18 +145,19 @@ ENGINE = InnoDB;
 -- Table `comidinhas_rapidas`.`pedido_produto`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `comidinhas_rapidas`.`pedido_produto` (
-  `pedido_id_pedido` INT NOT NULL,
-  `produto_id_produto` INT NOT NULL,
   `id_pedido_produto` INT NOT NULL AUTO_INCREMENT,
+  `pedido_id_pedido` INT NOT NULL,
+  `quantidade` INT NOT NULL,
+  `produto_id_produto` INT NOT NULL,
   PRIMARY KEY (`id_pedido_produto`),
-  INDEX `fk_pedido_has_produto_produto1_idx` (`produto_id_produto` ASC),
-  INDEX `fk_pedido_has_produto_pedido1_idx` (`pedido_id_pedido` ASC),
-  CONSTRAINT `fk_pedido_has_produto_pedido1`
+  INDEX `fk_pedido_has_produto_produto_idx` (`produto_id_produto` ASC),
+  INDEX `fk_pedido_has_produto_pedido_idx` (`pedido_id_pedido` ASC),
+  CONSTRAINT `fk_pedido_has_produto_pedido`
     FOREIGN KEY (`pedido_id_pedido`)
     REFERENCES `comidinhas_rapidas`.`pedido` (`id_pedido`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_pedido_has_produto_produto1`
+  CONSTRAINT `fk_pedido_has_produto_produto`
     FOREIGN KEY (`produto_id_produto`)
     REFERENCES `comidinhas_rapidas`.`produto` (`id_produto`)
     ON DELETE NO ACTION
